ce3b3a97d84c23713c96cd61cd8dee9bc86acd1c
Keenan Johnson <keenan.johnson@gmail.com>
2014-04-14 19:45:54 -0500

    Non working webserver base station with SD card read

--- Code/Server/Server.ino
+++ Code/Server/Server.ino
@@ -12,6 +12,7 @@
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
 #include "WebServer.h"
 
 /*------------------------------------------------------------------------
@@ -79,20 +80,43 @@ IPAddress ip(255, 255, 255, 255);
 
 EthernetClient old_client;
 
+File webFile; 
+
 /*------------------------------------------------------------------------
 FUNCTION PROTOTYPES
 ------------------------------------------------------------------------*/
 void print_local_ip_address();
 void save_local_ip_address();
 int send_cmd_to_client( const int &id, const node_cmds &cmd, const byte &data, EthernetClient* client );
-void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool);
+void Cmd_Center(WebServer &Web_server, WebServer::ConnectionType type, char *, bool);
 
 /*------------------------------------------------------------------------
 SETUP
 ------------------------------------------------------------------------*/
 void setup() {
+  
+  // disable Ethernet chip
+  // presumably to prevent SPI bus
+  // conflicts
+    pinMode(10, OUTPUT);
+    digitalWrite(10, HIGH);
+  
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
 
   // start the Ethernet connection:
   while(Ethernet.begin(mac) == 0) {
@@ -103,11 +127,7 @@ void setup() {
   
     /* setup our default command that will be run when the user accesses
    * the root page on the server */
-  webserver.setDefaultCommand(&helloCmd);
-
-  /* run the same command if you try to load /index.html, a common
-   * default page name */
-  webserver.addCommand("index.html", &helloCmd);
+  webserver.setDefaultCommand(&Cmd_Center);
 
   /* start the webserver */
   webserver.begin();
@@ -129,17 +149,19 @@ LOOP
 ------------------------------------------------------------------------*/
 void loop() {
   
-  char buff[64];
-  int len = 64;
+  #define web_buf_size 64
+  
+  char web_buff[web_buf_size];
+  int web_buf_len = web_buf_size;
+  
+  /* process incoming connections one at a time forever */
+  webserver.processConnection(web_buff, &web_buf_len);
   
   // Broadcast IP address
   UDP.beginPacket( ip, localPort );
   UDP.write( IP_addr, 4 );
   UDP.endPacket();
   Serial.println("Sending IP address");
-  
-    /* process incoming connections one at a time forever */
-  webserver.processConnection(buff, &len);
 
   // wait for a new client:
   EthernetClient client = server.available();
@@ -224,14 +246,15 @@ int send_cmd_to_client( const int &id, const node_cmds &cmd, const byte &data, E
   return (*client).write( (byte*)&packet, sizeof( packet ) );
 }
 
-/* commands are functions that get called by the webserver framework
- * they can read any posted data from client, and they output to the
- * server to send data back to the web browser. */
-void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
+// This is the function that process all incoming web connections
+// for now. It posts the http page below
+void Cmd_Center(WebServer &Web_server, WebServer::ConnectionType type, char *, bool)
 {
+  Serial.println("Connection received bixxxxxxx");
+  
   /* this line sends the standard "we're all OK" headers back to the
      browser */
-  server.httpSuccess();
+  Web_server.httpSuccess();
 
   /* if we're handling a GET or POST, we can output our data here.
      For a HEAD request, we just stop after outputting headers. */
@@ -240,9 +263,15 @@ void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
     /* this defines some HTML text in read-only memory aka PROGMEM.
      * This is needed to avoid having the string copied to our limited
      * amount of RAM. */
-    P(helloMsg) = "<h1>Hello, World Boop!</h1>";
-
-    /* this is a special form of print that outputs from PROGMEM */
-    server.printP(helloMsg);
+   // P(helloMsg) = "<h1>Hello, World Bop!</h1>";
+    
+    // send web page
+    webFile = SD.open("index.htm");        // open web page file
+    if (webFile) {
+        while(webFile.available()) {
+            Web_server.write(webFile.read()); // send web page to client
+        }
+        webFile.close();
+    }
   }
 }
